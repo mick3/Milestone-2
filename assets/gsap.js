f4bed65f28f919ebc1deb67e0d6eed8f5684edbc
mick3 <mvwebber3@gmail.com>
2020-05-26 14:08:25 -0400

    changed map.html to have a search box

--- assets/gsap.js
+++ assets/gsap.js
@@ -1,7 +1,7 @@
 var tl = gsap.timeline ({defaults:{opacity:0, ease:"back"}});
     
 function init () {
-    tl.from(".globe", {ease:"linear"})
+    tl.from(".globe", {ease:"linear", autoAlpha:0})
     tl.from(".subtitle", {x:80, duration:1}, "<")
     tl.from(".body", {x:-80}, "-=0.2")
     tl.from(".lead", {y:30, opacity:0}, "-=0.4")
