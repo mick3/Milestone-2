a261a9114c7ed7f53b1235dba6bb2c9f47abba35
mick3 <mvwebber3@gmail.com>
2020-05-31 12:41:59 -0400

    changed map type from terrain to roadmap in maps.js

--- assets/maps.js
+++ assets/maps.js
@@ -8,10 +8,14 @@
 
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 2.8,-187.3},
+      zoom: 2,
+      mapTypeId: 'map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
         center: new google.maps.LatLng(2.8,-187.3),
-        mapTypeId: 'terrain'
-      });
+        mapTypeId: 'roadmap'
+      });'
+    });
   
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
