a905f4b175eea9ffc4d8ee76da2031fcfacb9dcb
mick3 <mvwebber3@gmail.com>
2020-06-10 13:39:20 -0400

    changed map zoom to 10 and long lat coordinates to 0

--- assets/maps.js
+++ assets/maps.js
@@ -8,8 +8,8 @@
 
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 2,
+    center: {lat: 0, lng: 0},
+    zoom: 10,
     mapTypeId: 'roadmap'
   });
 
