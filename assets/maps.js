705be815a5275a00aa167b24c2e700d478622226
mick3 <mvwebber3@gmail.com>
2020-06-10 09:30:36 -0400

    changed map zoom to 2 in maps.js

--- assets/maps.js
+++ assets/maps.js
@@ -9,7 +9,7 @@
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
+    zoom: 2,
     mapTypeId: 'roadmap'
   });
 
