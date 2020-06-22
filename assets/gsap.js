41525f10b8067cc2ed0c5eb57a4e9080c94c1d72
mick3 <mvwebber3@gmail.com>
2020-06-22 17:23:22 -0400

    formatted files

--- assets/gsap.js
+++ assets/gsap.js
@@ -1,12 +1,12 @@
-var tl = gsap.timeline ({defaults:{opacity:0, ease:"back"}});
-    
-function init () {
-    tl.from(".globe", {ease:"linear", autoAlpha:0, duration:0.5})
-    tl.from(".subtitle", {x:80, duration:1}, "<")
-    tl.from(".body", {x:-80}, "-=0.2")
-    tl.from(".lead", {y:30, opacity:0}, "-=0.4")
+var tl = gsap.timeline({ defaults: { opacity: 0, ease: "back" } });
+
+function init() {
+    tl.from(".globe", { ease: "linear", autoAlpha: 0, duration: 0.5 })
+    tl.from(".subtitle", { x: 80, duration: 1 }, "<")
+    tl.from(".body", { x: -80 }, "-=0.2")
+    tl.from(".lead", { y: 30, opacity: 0 }, "-=0.4")
 }
 
-window.addEventListener("load", function(event){
+window.addEventListener("load", function (event) {
     init();
 });
